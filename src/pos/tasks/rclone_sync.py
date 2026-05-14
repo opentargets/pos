0afd2ea5e6dd20078ba3dc90e65b06da69ec4925
James Hayhurst <hayhurst.jd@gmail.com>
2026-05-14 15:28:42 +0100

    Update rclone task with include patterns

--- src/pos/tasks/rclone_sync.py
+++ src/pos/tasks/rclone_sync.py
@@ -14,11 +14,16 @@ class RcloneSyncSpec(Spec):
     This task has the following custom configuration fields:
         - source (str): The gcs path to sync from.
         - destination (str): The aws path to sync to.
+        - config (str): The path to the rclone config file.
+        - include (str): The path to the include file.
+        - sync_flags (str): The sync flags to pass to rclone.
     """
 
     source: str
     destination: str
     config: str = 'config/rclone.conf'
+    include: str = 'config/aws_sync.txt'
+    sync_flags: str = ''
 
 
 class RcloneSync(Task):
@@ -29,6 +34,16 @@ class RcloneSync(Task):
 
     @report
     def run(self) -> Task:
-        sync = Sync(Path(self.spec.source), Path(self.spec.destination), Path(self.spec.config))
-        sync.sync()
+        sync = Sync(
+            Path(_remove_storage_prefix(self.spec.source)),
+            Path(_remove_storage_prefix(self.spec.destination)),
+            Path(self.spec.config),
+            Path(self.spec.include),
+        )
+        flags = self.spec.sync_flags.split(' ') if self.spec.sync_flags else []
+        sync.sync(sync_flags=flags)
         return self
+
+
+def _remove_storage_prefix(source: str) -> str:
+    return source.replace('gs://', '').replace('s3://', '')
