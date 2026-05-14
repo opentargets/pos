054289c20c3841de1239f01b54c381ce1c39089d
James Hayhurst <hayhurst.jd@gmail.com>
2026-05-14 15:28:42 +0100

    Update rclone task with include patterns

--- src/pos/rclone/sync.py
+++ src/pos/rclone/sync.py
@@ -8,16 +8,31 @@ from loguru import logger
 class Sync:
     """A class to handle rclone sync operations."""
 
-    def __init__(self, source: Path, destination: Path, config: Path) -> None:
+    def __init__(self, source: Path, destination: Path, config: Path, include_file: Path) -> None:
         self.source = source
         self.destination = destination
         self.config = config
+        self.include_paths = include_file
 
-    def sync(self, dry_run: bool = False) -> CompletedProcess:
+    def sync(self, sync_flags: list[str]) -> CompletedProcess:
         """Syncs the source directory to the destination using rclone."""
         logger.info(f'Syncing {self.source} to {self.destination}')
-        return subprocess.run(
-            ['rclone', 'sync', '--config', str(self.config), str(self.source), str(self.destination)],
-            capture_output=True,
-            text=True,
-        )
+        command = [
+            'rclone',
+            '--config',
+            str(self.config),
+            'sync',
+            str(self.source),
+            str(self.destination),
+            *sync_flags,
+            '--include-from',
+            str(self.include_paths),
+        ]
+        logger.debug(f'Running command: {" ".join(command)}')
+        rclone = subprocess.run(command, capture_output=True, text=True)
+        if rclone.stdout:
+            logger.debug(rclone.stdout)
+        if rclone.stderr:
+            logger.error(rclone.stderr) if rclone.returncode != 0 else logger.debug(rclone.stderr)
+        rclone.check_returncode()
+        return rclone
