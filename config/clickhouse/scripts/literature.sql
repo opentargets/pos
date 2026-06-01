b96d6daeecc46a0aab885853384d917e2ebc81bb
ricardo <remo87@gmail.com>
2026-06-01 14:56:37 +0100

    update lit table name

--- config/clickhouse/scripts/literature.sql
+++ config/clickhouse/scripts/literature.sql
@@ -1,12 +1,12 @@
-CREATE TABLE IF NOT EXISTS literature ENGINE = MergeTree ()
+CREATE TABLE IF NOT EXISTS literature_entity_lut ENGINE = MergeTree ()
 ORDER BY (
         keywordId, year, month, relevance
     ) AS (
         SELECT
             pmid, pmcid, keywordId, relevance, date, year, month
-        FROM literature_log
+        FROM literature_entity_lut_log
     );
 
 OPTIMIZE TABLE literature FINAL;
 
-DROP TABLE literature_log SYNC;
+DROP TABLE literature_entity_lut_log SYNC;
